c009c2ade59cd38857e446b2b672166ea06f6153
Vraj Patel <vrajpatel@Vrajs-MacBook-Pro-2.local>
2020-01-31 15:55:43 -0500

    Adding remaining files

--- app/User.php
+++ app/User.php
@@ -41,6 +41,7 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTIdentifier(){
         return $this->getKey();
     }
+    
     public function getJWTCustomClaims(){
         return [];
     }
